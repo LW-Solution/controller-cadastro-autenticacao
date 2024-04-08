b5cbfa26d060fadf7796ad59325e6b70802c11bf
Miguel Carvalho <doxito007@gmail.com>
2024-04-08 08:49:31 -0300

    Ajustando configurações das rotas

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { AppDataSource } from "./data-source"
-
+import { AppDataSource } from "./data-source";
+import  router  from './routes'; 
 
 dotenv.config();
 
 const app = express();
 
-AppDataSource.initialize()
+app.use(express.json());
+
+AppDataSource.initialize();
+
+app.use(router);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);

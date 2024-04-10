e1222357214b45feb12405b5c11a0e70f5622c54
Miguel <doxito007@gmail.com>
2024-04-10 09:48:48 -0300

    index.ts Importação das rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { default as User} from "./user";
+import { default as Permission} from "./permission";
 const router = Router();
 
 router.use("/user",User);
+router.use("/permission",Permission);
 
 export default router;
